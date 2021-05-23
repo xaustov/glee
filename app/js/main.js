dd790133145898c4cfd736d9c716dc761e2671e5
xaustov <xaustov@gmail.com>
2021-05-24 00:21:03 +0300

    added product-details

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,48 @@
 $(function(){
+
+  $('.product-tabs__link').on('click', function (e){
+    e.preventDefault();
+    $('.product-tabs__link').removeClass('product-tabs__link--active');
+    $(this).addClass('product-tabs__link--active');
+
+    $('.product-tabs__content-item').removeClass('product-tabs__content-item--active');
+    $($(this).attr('href')).addClass('product-tabs__content-item--active');
+  });
+  
+  
+  
+  
+  $('.related__slider').slick({
+    slidesToShow: 4,
+    // appendArrows: $('.related__slider-btn--left, .related__slider-btn--right'),
+    prevArrow: $('.related__slider-btn--left'),
+    nextArrow: $('.related__slider-btn--right')
+  });
+  
+  $('.top-slider__inner').slick({
+    dots: true,
+    arrows: false,
+    autoplay: false,
+    autoplaySpeed: 2000
+  });
+  
+  $('.details__thumbs').slick({
+    asNavFor: '.details__big',
+    focusOnSelect: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    vertical: true,
+    draggable: false
+  });
+  $('.details__big').slick({
+    asNavFor: '.details__thumbs',
+    draggable: false,
+    arrows: false,
+    fade: true
+  });
+
+
+  $('.details__form-input').styler();
   
   $('.shop-list__filter-btn').on('click', function () {
     $('.shop-list__filter-btn').removeClass('shop-list__filter-btn--active');
@@ -56,13 +100,17 @@ $(function(){
     ratedFill: "#ffcc00",
     normalFill: "#d6d6d6"
   });
+
+  $('.details__star-rating').rateYo({
+    rating: 4,
+    readOnly: true,
+    starWidth: "15px",
+    spacing: "10px",
+    ratedFill: "#ffcc00",
+    normalFill: "#d6d6d6"
+  });
   
-  $('.top-slider__inner').slick({
-    dots: true,
-    arrows: false,
-    autoplay: false,
-    autoplaySpeed: 2000
- });
+  
  
   
   
--- gulpfile.js
+++ gulpfile.js
@@ -36,6 +36,7 @@ function scripts() {
     'node_modules/mixitup/dist/mixitup.js',
     'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
     'node_modules/rateyo/src/jquery.rateyo.js',
+    'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
     'app/js/main.js'
   ])
     .pipe(concat('main.min.js'))
