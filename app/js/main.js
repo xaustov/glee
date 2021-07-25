fb67dd1c36e1d43a38b1af297a7c0c02e9fd4f46
xaustov <xaustov@gmail.com>
2021-07-25 22:42:51 +0300

    added footer adaptiv

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,8 @@
 $(function(){
 
+  $('.menu__btn').on('click', function (){
+    $('.menu__list').toggleClass('menu__list--active')
+  });
   
 
   $('.product-tabs__link').on('click', function (e){
